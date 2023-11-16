4669296ac9ee84846c9221ae635d3e8f79b9b792
habib-153 <h.r.sihab155@gmail.com>
2023-11-16 15:52:58 +0600

    user can delete item from cart

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 //bistroBoss
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.29d8nwh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,6 +49,13 @@ async function run() {
       const email = req.query.email
       const query = { email: email}
       const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
     // Send a ping to confirm a successful connection

488479d560bcf6dc38a4343fc7807b613ecfaeec
habib-153 <h.r.sihab155@gmail.com>
2023-11-17 14:48:58 +0600

    can delete user from database

--- index.js
+++ index.js
@@ -53,6 +53,18 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // ------------------------------
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray()

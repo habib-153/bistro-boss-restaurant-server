e57816c8597b65d62931a79cbcf6fc0146ee6a34
habib-153 <h.r.sihab155@gmail.com>
2023-11-21 01:02:15 +0600

    admin-stats

--- index.js
+++ index.js
@@ -238,6 +238,27 @@ async function run() {
       res.send(result)
     })
 // -----------------------------
+// Stats and Analytics
+    app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res) =>{
+      const users = await userCollection.estimatedDocumentCount()
+      const menuItems = await menuCollection.estimatedDocumentCount()
+      const orders = await paymentCollection.estimatedDocumentCount()
+      const result = await paymentCollection.aggregate([
+        {
+          $group:{
+            _id: null,
+            totalRevenue:{
+              $sum: '$price'
+            }
+          }
+        }
+      ]).toArray()
+      const revenue = result.length > 0 ? result[0].totalRevenue :0 ;
+      res.send({
+        users, menuItems, orders, revenue
+      })
+    })
+// -----------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

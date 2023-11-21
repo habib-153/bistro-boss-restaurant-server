8b57f74b25c1e5a898329306b0d4cb32cce2692b
habib-153 <h.r.sihab155@gmail.com>
2023-11-22 01:06:44 +0600

    api for chart created

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ async function run() {
         $in: payment.cartIds.map(id => new ObjectId(id))
       }}
       const deleteResult = await cartCollection.deleteMany(query)
-      res.send(paymentResult, deleteResult)
+      res.status(200).send([paymentResult, deleteResult])
     })
 
     app.get('/payments/:email', verifyToken, async(req, res)=>{
@@ -258,6 +258,41 @@ async function run() {
         users, menuItems, orders, revenue
       })
     })
+  // using aggregate pipeline
+  app.get('/order-stats',verifyToken, verifyAdmin, async(req, res)=>{
+    const result = await paymentCollection.aggregate([
+      {
+        $unwind: '$menuItemIds'
+      },
+      {
+        $lookup: {
+          from: 'menu',
+          localField: 'menuItemIds',
+          foreignField: '_id',
+          as: 'menuItems'
+        }
+      },
+      {
+        $unwind: '$menuItems'
+      },
+      {
+        $group:{
+          _id: '$menuItems.category',
+          quantity: {$sum: 1},
+          revenue: { $sum: '$menuItems.price'}
+        }
+      },
+      {
+        $project:{
+          _id: 0,
+          category: '$_id',
+          quantity: '$quantity',
+          revenue: '$revenue'
+        }
+      }
+    ]).toArray()
+    res.send(result)
+  })
 // -----------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
